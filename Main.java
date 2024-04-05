555949d6d3545e31a556d874d69e9eb3bec53650
sassanghz <sassanghazi@gmail.com>
2024-04-05 17:40:32 -0400

    displayMenu

--- Main.java
+++ Main.java
@@ -1,20 +1,7 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
-        System.out.println("---------------------------------");
-        System.out.println("    Vocabulary Control Center ");
-        System.out.println("---------------------------------");
-        System.out.println("1  browse a topic");
-        System.out.println("2  insert a new topic before another one");
-        System.out.println("3  insert a new topic after another one");
-        System.out.println("4  remove a topic");
-        System.out.println("5  modify a topic");
-        System.out.println("6  search topics for a word");
-        System.out.println("7  load from a file");
-        System.out.println("8  show all words starting with a given letter");
-        System.out.println("9  save to file");
-        System.out.println("0  exit");
-        System.out.println("---------------------------------");
+       
         System.out.println("Enter Your Choice: ");
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
@@ -96,5 +83,22 @@ public class Main{
 
         sc.close();
 
+    }
+
+    private static void displayMenu(){
+        System.out.println("---------------------------------");
+        System.out.println("   Vocabulary Control Center");
+        System.out.println("---------------------------------");
+        System.out.println("1  browse a topic");
+        System.out.println("2  insert a new topic before another one");
+        System.out.println("3  insert a new topic after another one");
+        System.out.println("4  remove a topic");
+        System.out.println("5  modify a topic");
+        System.out.println("6  search topics for a word");
+        System.out.println("7  load from a file");
+        System.out.println("8  show all words starting with a given letter");
+        System.out.println("9  save to file");
+        System.out.println("0  exit");
+        System.out.println("---------------------------------");
     }
 }
