4efe6f942a61b13da743ae128b6657d03915ebe5
sassanghz <sassanghazi@gmail.com>
2024-04-05 16:54:10 -0400

    added menu

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+
 public class Main{
     public static void main(String[] args){
-        
+        System.out.println("---------------------------------");
+        System.out.println("\t + Vocabulary Control Center ");
+        System.out.println("---------------------------------");
+        System.out.println("1  browse a topic");
+        System.out.println("2  insert a new topic before another one");
+        System.out.println("3  insert a new topic after another one");
+        System.out.println("4  remove a topic");
+        System.out.println("5  modify a topic");
+        System.out.println("6  search topics for a word");
+        System.out.println("7  load from a file");
+        System.out.println("8  show all words starting with a given letter");
+        System.out.println("9  save to file");
+        System.out.println("0  exit");
+        System.out.println("---------------------------------");
+        System.out.println("Enter Your Choice: ");
+
     }
 }
