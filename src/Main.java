def7e1c219acffefd0b5854875673b4fabb88d38
sassanghz <sassanghazi@gmail.com>
2024-04-06 00:53:13 -0400

    new Methods

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-import java.util.LinkedList;
+package src;
 import java.util.Scanner;
 public class Main{
 
-    private static LinkedList<Vocab> vocabList = new LinkedList<>();
     private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
        
@@ -15,31 +14,31 @@ public class Main{
 
             switch (choice) {
                 case 1:
-                    browseTopic();
+                    Methods.browseTopic();
                     break;
                 case 2:
-                    insertTopicBefore();
+                    Methods.insertTopicBefore();
                     break;
                 case 3:
-                    insertTopicAfter();
+                    Methods.insertTopicAfter();
                     break;
                 case 4:
-                    removeTopic();
+                    Methods.removeTopic();
                     break;
                 case 5:
-                    modifyTopic();
+                    Methods.modifyTopic();
                     break;
                 case 6:
-                    searchTopicsForWord();
+                    Methods.searchTopicsForWord();
                     break;
                 case 7:
-                    loadFromFile();
+                    Methods.loadFromFile();
                     break;
                 case 8:
-                    showWordsStartingWithLetter();
+                    Methods.showWordsStartingWithLetter();
                     break;
                 case 9:
-                    saveToFile();
+                    Methods.saveToFile();
                     break;
                 case 0:
                     System.out.println("Exiting program...");
@@ -148,38 +147,4 @@ public class Main{
         System.out.println("---------------------------------");
     }
 
-    private static void browseTopic() {
-        System.out.println("---------------------------");
-        System.out.println("      Pick a topic ");
-        System.out.println("---------------------------");
-        int index = 1;
-        for (Vocab vocab : vocabList) {
-            System.out.println(index + ". " + vocab.getTopic());
-            index++;
-        }
-        System.out.println("0. Exit");
-        System.out.print("Enter Your Choice: ");
-        int choice = sc.nextInt();
-        if (choice > 0 && choice <= vocabList.size()) {
-            System.out.println(vocabList.get(choice - 1));
-        } else if (choice != 0) {
-            System.out.println("Invalid choice.");
-        }
-    }
-
-    private static void insertTopicBefore(){}
-
-    private static void insertTopicAfter(){}
-
-    private static void removeTopic(){}
-
-    private static void modifyTopic(){}
-
-    private static void searchTopicsForWord(){}
-
-    private static void loadFromFile(){}
-
-    private static void showWordsStartingWithLetter(){}
-
-    private static void saveToFile(){}
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,4 +1,43 @@
 package src;
+import java.util.LinkedList;
+import java.util.Scanner;
 public class Methods {
+
+    public static LinkedList<Vocab> vocabList = new LinkedList<>();
+    public static Scanner sc = new Scanner(System.in);
     
+    public static void browseTopic() {
+        System.out.println("---------------------------");
+        System.out.println("      Pick a topic ");
+        System.out.println("---------------------------");
+        int index = 1;
+        for (Vocab vocab : vocabList) {
+            System.out.println(index + ". " + vocab.getTopic());
+            index++;
+        }
+        System.out.println("0. Exit");
+        System.out.print("Enter Your Choice: ");
+        int choice = sc.nextInt();
+        if (choice > 0 && choice <= vocabList.size()) {
+            System.out.println(vocabList.get(choice - 1));
+        } else if (choice != 0) {
+            System.out.println("Invalid choice.");
+        }
+    }
+
+    public static void insertTopicBefore(){}
+
+    public static void insertTopicAfter(){}
+
+    public static void removeTopic(){}
+
+    public static void modifyTopic(){}
+
+    public static void searchTopicsForWord(){}
+
+    public static void loadFromFile(){}
+
+    public static void showWordsStartingWithLetter(){}
+
+    public static void saveToFile(){}
 }
